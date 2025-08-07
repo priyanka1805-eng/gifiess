ce432f70c001701091ad9f8784bc42fe53aee37e
PRIYANKA <priyankadeshh@gmail.com>
2025-08-07 05:49:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/gifiess/", // ← Add this line for GitHub Pages
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
